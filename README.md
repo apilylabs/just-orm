0cecda527a78a19ecee105310ffbb55d1bbea1a4
code-xhyun <shhong@taling.me>
2024-06-07 22:11:20 +0900

    docs: update readme.md and annotation

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# rust-json-db
-rust-json-db
+<p align="center">
+    <h1 align="center">just-orm</h1>
+</p>
+<p align="center">
+    <em>A simple database ORM that use JSON file for Rust</em>
+</p>
+<br/>
+<br/>
+
+
+
+
+
+### Example
+```rust
+use just_orm::{Identifiable, JsonDatabase};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct User {
+    id: String,
+    name: String,
+    email: String,
+}
+
+impl Identifiable for User {
+    fn get_id(&self) -> String {
+        self.id.clone()
+    }
+}
+
+fn main() {
+    // Initialize the database
+    let mut user_db: JsonDatabase<User> = JsonDatabase::new(Some("users"));
+
+    // Example user data
+    let user1 = User {
+        id: "1".to_string(),
+        name: "John Doe".to_string(),
+        email: "john.doe@example.com".to_string(),
+    };
+
+    let user2 = User {
+        id: "2".to_string(),
+        name: "Jane Smith".to_string(),
+        email: "jane.smith@example.com".to_string(),
+    };
+
+    // Create users
+    user_db.create_model(user1);
+    user_db.create_model(user2);
+
+    // Find a user by ID
+    if let Some(user) = user_db.find_by_id("1") {
+        println!("Found user: {:?}", user);
+    } else {
+        println!("User not found");
+    }
+
+    // Update a user's information
+    let update_data = json!({
+        "name": "Johnathan Doe"
+    });
+    user_db.update_by_id("1", update_data);
+
+    // Find all users
+    let all_users = user_db.find_all();
+    println!("All users: {:?}", all_users);
+
+    // Find users by condition
+    let condition = json!({
+        "email": "jane.smith@example.com"
+    });
+    let found_users = user_db.find(&condition);
+    println!("Found users: {:?}", found_users);
+
+    // Delete a user by ID
+    user_db.delete_by_id("2");
+
+    // Find all users after deletion
+    let all_users_after_deletion = user_db.find_all();
+    println!("All users after deletion: {:?}", all_users_after_deletion);
+}
+
+```
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::path::{Path, PathBuf};
 
 const BASE_DIR: &str = "json-db";
 
+/// A simple JSON file-based database ORM for Rust.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct JsonDatabase<T> {
     base_path: PathBuf,
@@ -17,6 +18,17 @@ impl<T> JsonDatabase<T>
 where
     T: Serialize + for<'de> Deserialize<'de> + Clone + Identifiable,
 {
+    /// Creates a new `JsonDatabase` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `model_name` - Optional model name to initialize the database with.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let db: JsonDatabase<User> = JsonDatabase::new(Some("users"));
+    /// ```
     pub fn new(model_name: Option<&str>) -> Self {
         let base_path = if let Some(model_name) = model_name {
             let path = Path::new(BASE_DIR).join(model_name);
@@ -35,16 +47,38 @@ where
         }
     }
 
+    /// Sets the model name for the database and ensures the directory exists.
+    ///
+    /// # Arguments
+    ///
+    /// * `model_name` - The name of the model to use.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut db: JsonDatabase<User> = JsonDatabase::new(None);
+    /// db.model("users");
+    /// ```
     pub fn model(&mut self, model_name: &str) -> &mut Self {
         self.current_model_name = Some(model_name.to_string());
         create_directory_if_not_exists(&self.get_model_path(model_name));
         self
     }
 
+    /// Returns the path to the model directory.
+    ///
+    /// # Arguments
+    ///
+    /// * `model_name` - The name of the model.
     fn get_model_path(&self, model_name: &str) -> PathBuf {
         Path::new(BASE_DIR).join(model_name)
     }
 
+    /// Returns the path to a specific file in the model directory.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The ID of the file.
     fn get_file_path(&self, id: &str) -> PathBuf {
         let model_name = self
             .current_model_name
@@ -53,6 +87,7 @@ where
         self.get_model_path(model_name).join(format!("{}.json", id))
     }
 
+    /// Returns a list of all JSON files in the model directory.
     fn get_all_files(&self) -> Vec<String> {
         let model_name = self
             .current_model_name
@@ -76,6 +111,12 @@ where
             .collect()
     }
 
+    /// Checks if a JSON object matches a given condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `item` - The JSON object to check.
+    /// * `condition` - The condition to match against.
     fn matches_condition(&self, item: &Value, condition: &Value) -> bool {
         if !condition.is_object() || condition.is_null() {
             return item == condition;
@@ -95,11 +136,24 @@ where
         })
     }
 
+    /// Gets a nested property from a JSON object.
+    ///
+    /// # Arguments
+    ///
+    /// * `obj` - The JSON object.
+    /// * `keys` - The keys to the nested property.
     fn get_nested_property<'a>(&self, obj: &'a Value, keys: &[&str]) -> &'a Value {
         keys.iter()
             .fold(obj, |acc, key| acc.get(*key).unwrap_or(&Value::Null))
     }
 
+    /// Sets a nested property in a JSON object.
+    ///
+    /// # Arguments
+    ///
+    /// * `obj` - The JSON object.
+    /// * `keys` - The keys to the nested property.
+    /// * `value` - The value to set.
     fn set_nested_property(&self, obj: &mut Value, keys: &[&str], value: Value) {
         if keys.len() == 1 {
             obj[keys[0]] = value;
@@ -114,6 +168,12 @@ where
         }
     }
 
+    /// Updates a nested property in a JSON object.
+    ///
+    /// # Arguments
+    ///
+    /// * `target` - The JSON object to update.
+    /// * `source` - The source JSON object containing updates.
     fn update_nested_object(&self, target: &mut Value, source: &Value) {
         for (key, value) in source.as_object().unwrap().iter() {
             let keys: Vec<&str> = key.split('.').collect();
@@ -121,6 +181,15 @@ where
         }
     }
 
+    /// Creates a new model in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - The data to create.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the data does not have an ID field.
     pub fn create_model(&self, data: T) {
         let id = data.get_id();
         if id.is_empty() {
@@ -129,17 +198,37 @@ where
         self.create(&id, data);
     }
 
+    /// Creates a new record in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The ID of the record.
+    /// * `data` - The data to create.
     pub fn create(&self, id: &str, data: T) {
         let file_path = self.get_file_path(id);
         write_json_file(&file_path, &data);
     }
 
+    /// Finds a record by ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The ID of the record to find.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Some(T)` if the record is found, or `None` if not.
     pub fn find_by_id(&self, id: &str) -> Option<T> {
         let file_path = self.get_file_path(id);
-        println!("--aaaaaa-{:?}", file_path);
         read_json_file(&file_path)
     }
 
+    /// Updates a record by ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The ID of the record to update.
+    /// * `data` - The data to update.
     pub fn update_by_id(&self, id: &str, data: Value) {
         let file_path = self.get_file_path(id);
 
@@ -152,6 +241,11 @@ where
         }
     }
 
+    /// Deletes a record by ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The ID of the record to delete.
     pub fn delete_by_id(&self, id: &str) {
         let file_path = self.get_file_path(id);
         if file_path.exists() {
@@ -159,6 +253,11 @@ where
         }
     }
 
+    /// Finds all records.
+    ///
+    /// # Returns
+    ///
+    /// Returns a vector of all records.
     pub fn find_all(&self) -> Vec<T> {
         let files = self.get_all_files();
         files
@@ -173,6 +272,15 @@ where
             .collect()
     }
 
+    /// Finds records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    ///
+    /// # Returns
+    ///
+    /// Returns a vector of matching records.
     pub fn find(&self, condition: &Value) -> Vec<T> {
         self.find_all()
             .into_iter()
@@ -180,16 +288,40 @@ where
             .collect()
     }
 
+    /// Finds the first record matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Some(T)` if a matching record is found, or `None` if not.
     pub fn find_one(&self, condition: &Value) -> Option<T> {
         self.find_all()
             .into_iter()
             .find(|item| self.matches_condition(&serde_json::to_value(item).unwrap(), condition))
     }
 
+    /// Counts the number of records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    ///
+    /// # Returns
+    ///
+    /// Returns the number of matching records.
     pub fn count(&self, condition: &Value) -> usize {
         self.find(condition).len()
     }
 
+    /// Updates multiple records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    /// * `data` - The data to update.
     pub fn update_many(&self, condition: &Value, data: &Value) {
         let items = self.find(condition);
         for item in items {
@@ -198,6 +330,11 @@ where
         }
     }
 
+    /// Deletes multiple records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
     pub fn delete_many(&self, condition: &Value) {
         let items = self.find(condition);
         for item in items {
@@ -206,6 +343,13 @@ where
         }
     }
 
+    /// Adds an element to an array in records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    /// * `array_path` - The path to the array.
+    /// * `element` - The element to add.
     pub fn push(&self, condition: &Value, array_path: &str, element: &Value) {
         let items = self.find(condition);
         for item in items {
@@ -225,6 +369,13 @@ where
         }
     }
 
+    /// Removes elements from an array in records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    /// * `array_path` - The path to the array.
+    /// * `pull_condition` - The condition to match elements to remove.
     pub fn pull(&self, condition: &Value, array_path: &str, pull_condition: &Value) {
         let items = self.find(condition);
         for item in items {
@@ -249,6 +400,14 @@ where
         }
     }
 
+    /// Updates elements in an array in records matching a condition.
+    ///
+    /// # Arguments
+    ///
+    /// * `condition` - The condition to match.
+    /// * `array_path` - The path to the array.
+    /// * `array_condition` - The condition to match array elements.
+    /// * `updates` - The updates to apply to matching elements.
     pub fn update_array(
         &self,
         condition: &Value,
@@ -288,12 +447,26 @@ where
     }
 }
 
+/// Creates a directory if it does not exist.
+///
+/// # Arguments
+///
+/// * `path` - The path of the directory to create.
 fn create_directory_if_not_exists(path: &Path) {
     if !path.exists() {
         fs::create_dir_all(path).expect("Unable to create directory");
     }
 }
 
+/// Reads a JSON file and deserializes it into a Rust struct.
+///
+/// # Arguments
+///
+/// * `path` - The path of the JSON file to read.
+///
+/// # Returns
+///
+/// Returns `Some(T)` if successful, or `None` if there is an error.
 fn read_json_file<T>(path: &Path) -> Option<T>
 where
     T: for<'de> Deserialize<'de>,
@@ -304,6 +477,12 @@ where
     from_str(&contents).ok()
 }
 
+/// Serializes a Rust struct into JSON and writes it to a file.
+///
+/// # Arguments
+///
+/// * `path` - The path of the file to write.
+/// * `data` - The data to serialize and write.
 fn write_json_file<T>(path: &Path, data: &T)
 where
     T: Serialize,
@@ -314,6 +493,8 @@ where
         .expect("Unable to write to file");
 }
 
+/// A trait for types that have an ID.
 pub trait Identifiable {
+    /// Returns the ID of the object.
     fn get_id(&self) -> String;
 }
